d0f6f2de34c434075ba34a100c6949c45501fa6d
Craig <craigthompson85@gmail.com>
2024-05-13 13:41:18 -0600

    Add state variable for showing pictures on cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
 import playerData from "./playerData.js";
+import { useState } from "react";
 
 function BaseballCard(props) {
-  // return (
-  //   <div className="card">
-  //     <h2>{props.name}</h2>
-  //     <img src={props.imgUrl} alt="" />
-  //   </div>
-  // );
-  console.log(Object.entries(props.stats));
-  const statsDisplay = Object.entries(props.stats).map(([stat, value]) => (
-    <p>
-      {stat}: {props.stats[stat]}
-    </p>
-  ));
-  return (
-    <div className="card">
-      <h2>{props.name}</h2>
-      <p>Team: {props.team}</p>
-      <p>Position: {props.position}</p>
-      {statsDisplay}
-    </div>
-  );
+  const [showPicture, setShowPicture] = useState(true);
+
+  if (showPicture) {
+    return (
+      <div className="card">
+        <h2>{props.name}</h2>
+        <img src={props.imgUrl} alt="" />
+      </div>
+    );
+  } else {
+    const statsDisplay = Object.entries(props.stats).map(([stat, value]) => (
+      <p>
+        {stat}: {props.stats[stat]}
+      </p>
+    ));
+    return (
+      <div className="card">
+        <h2>{props.name}</h2>
+        <p>Team: {props.team}</p>
+        <p>Position: {props.position}</p>
+        {statsDisplay}
+      </div>
+    );
+  }
 }
 
 function App() {
